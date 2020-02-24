278d174bb34bd7a07786529004067137679071a9
azadeharam <aram.azadeh@yahoo.com>
2020-02-24 18:10:05 +0100

    puts missin devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :plants, only: [:index, :show, :new, :create] do
+    resources :bookings, only: [:create]
+    resources :users, only: [:index, :show]
+  end
+
+  resources :bookings, only: [:destroy]
 
 end
