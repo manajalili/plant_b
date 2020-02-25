826e0fe564bb2c0f6c19638704dc187fdd5a30e7
azadeharam <aram.azadeh@yahoo.com>
2020-02-25 12:25:54 +0100

    booking_controller and show.html.erb is done

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,22 @@
+class BookingsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    plant = Plant.find(params[:plant_id])
+    today = Date.today
+    bookings = plant.bookings.where("start_date >= ? OR end_date >= ?", today, today)
+
+    render json: bookings
+  end
+
+  def create
+    @booking = current_user.bookings.create(booking_params)
+
+    redirect_to @booking.plant, notice: "Your booking has been created..."
+  end
+
+  private
+    def booking_params
+      params.require(:booking).permit(:start_date, :end_date, :price, :total, :plant_id)
+    end
+end
