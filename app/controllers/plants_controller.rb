fc35a3f7fbd1013e61e9c6846fe4f34e355800f3
mana jalili <jalilimana@gmail.com>
2020-02-25 14:09:40 +0100

    done with new, create, show and their views but simply view, still the design is missing

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -2,4 +2,29 @@ class PlantsController < ApplicationController
   def index
     @plants = Plant.all
   end
+
+  def show
+    @plant = Plant.find(params[:id])
+    @bookings = @plant.bookings
+  end
+
+  def new
+    @plant = Plant.new
+  end
+
+  def create
+    @plant = Plant.new(plant_params)
+    @plant.user = current_user
+    if @plant.save!
+      redirect_to plant_path(@plant)
+    else
+      render :new
+    end
+  end
+
+  private
+  def plant_params
+    params.require(:plant).permit(:name, :description)
+  end
+
 end
