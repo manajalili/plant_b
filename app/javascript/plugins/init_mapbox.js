4226f62259acde8eb4d7727f54c92f3c19b7222f
azadeharam <aram.azadeh@yahoo.com>
2020-02-26 18:50:26 +0100

    map with mark is fixed

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,42 +1,23 @@
-
-<<<<<<< HEAD
 import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
-const mapElement = document.getElementById('map');
-
-const buildMap = () => {
-  mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-  return new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v10'
-  });
-};
-
-const addMarkersToMap = (map, markers) => {
-  markers.forEach((marker) => {
-    new mapboxgl.Marker()
-      .setLngLat([ marker.lng, marker.lat ])
-      .addTo(map);
-  });
-};
-=======
-const initMapbox = () => {
-  const mapElement = document.getElementById('map');
->>>>>>> 287f5ff0f5ea622f8d68be015a8737b90a21ae41
-
-const fitMapToMarkers = (map, markers) => {
-  const bounds = new mapboxgl.LngLatBounds();
-  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
-};
 
 const initMapbox = () => {
-  if (mapElement) {
-    const map = buildMap();
-    const markers = JSON.parse(mapElement.dataset.markers);
-    addMarkersToMap(map, markers);
-    fitMapToMarkers(map, markers);
+  const mapElement = document.getElementById('map');const fitMapToMarkers = (map, markers) => {
+
+const bounds = new mapboxgl.LngLatBounds();
+markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+};  if (mapElement) { // only build a map if there's a div#map to inject into
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v10'
+    });    const markers = JSON.parse(mapElement.dataset.markers);
+    markers.forEach((marker) => {
+      new mapboxgl.Marker()
+        .setLngLat([ marker.lng, marker.lat ])
+        .addTo(map);
+    });    fitMapToMarkers(map, markers);
   }
-};
-
-export { initMapbox };
+};export { initMapbox };
