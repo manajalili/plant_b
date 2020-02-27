ff79de97e83914f0e056ef7cba0287b45e0d16ea
mason tan <masonev@gmail.com>
2020-02-27 18:42:29 +0100

    Added auto-complete+dropdown select search bar on the mainpage and navbar, added icon to tab and named

--- app/javascript/components/init_select2.js
+++ app/javascript/components/init_select2.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import 'select2';
 
 const initSelect2 = () => {
-  $('.js-exammple-basic-single').select2({
+  $('.js-exammple-basic-single').val('').select2({
     placeholder: "Find a plant",
     allowClear: true
   });
