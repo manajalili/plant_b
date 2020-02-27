1979ccceea619e3db58111f9d8b70a4e03553c2a
mana jalili <jalilimana@gmail.com>
2020-02-27 17:03:45 +0100

    dashboard design done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -13,12 +13,31 @@ class BookingsController < ApplicationController
     @booking.plant = @plant
     # Find the user
     @booking.user = current_user
+    if @booking.save
+      redirect_to @booking.plant, notice: "Your booking has been created..."
+    else
+      render :new
+    end
+  end
 
-    @booking.save!
+  def edit
+    @booking = Booking.find(params[:id])
+  end
 
+  def update
+    @plant = Plant.find(params[:plant_id])
+    @booking = Booking.find(params[:id])
+    @booking.update(booking_params)
+    raise
 
+    redirect_to plant_path(@booking.plant)
+  end
 
-    redirect_to @booking.plant, notice: "Your booking has been created..."
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+
+    redirect_to bookings_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :plants do
-    resources :bookings, only: [:create]
+    resources :bookings, only: [:create, :update]
     resources :users, only: [:index, :show]
   end
 
-  resources :bookings, only: [:index, :update, :destroy]
+  resources :bookings, only: [:index, :destroy]
 end
