4f759590d6b00f8506894a9b7696e8e2b87aa8eb
mana jalili <jalilimana@gmail.com>
2020-02-27 13:20:20 +0100

    added the index, update and destroy inside route file

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
     resources :users, only: [:index, :show]
   end
 
-  resources :bookings, only: [:destroy]
+  resources :bookings, only: [:index, :update, :destroy]
 end
