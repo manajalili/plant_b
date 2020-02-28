25ede9d7c04284df6b3b058248e295ab3820c5ea
mana jalili <jalilimana@gmail.com>
2020-02-28 12:38:35 +0100

    removed body

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :plants do
-    resources :bookings, only: [:create, :update, :edit]
+    resources :bookings, only: [:create]
     resources :users, only: [:index, :show]
   end
+
+  resources :bookings, only: [:edit, :update]
 
   resources :bookings, only: [:index, :destroy]
 end
