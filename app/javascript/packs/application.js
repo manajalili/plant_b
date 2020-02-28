4d5b3f8fed372076c5e7193e4396774d243b7b2e
mana jalili <jalilimana@gmail.com>
2020-02-28 15:34:09 +0100

    add mapbox link to their servers

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,10 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-import 'mapbox-gl/dist/mapbox-gl.css';
+// import 'mapbox-gl/dist/mapbox-gl.css';
 import initMapbox from '../plugins/init_mapbox';
 import initSelect2 from '../plugins/init_select2';
 import initEntersearch from '../plugins/init_entersearch';
-// import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
 initMapbox();
 
